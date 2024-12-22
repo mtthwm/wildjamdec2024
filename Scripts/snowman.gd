365d9682b9e9f09705599485630dc5176f406a46
joseph-youngblood <joseph@youngbloods.org>
2024-12-22 02:20:52 -0700

    Added snowmen and baby/boss gingerbread

--- Scripts/snowman.gd
+++ Scripts/snowman.gd
@@ -1,22 +1,20 @@
 extends CharacterBody3D
 
 
-var SPEED
-var health
-@export var baby = false
-@export var boss = false
+const SPEED = 0.02
+const ATTACK_DELAY = 1
+var health = 2
 
+@onready var timer: Timer = $"Timer"
+@onready var skater: CharacterBody3D = $"../../skater"
+
+@onready var shooter: RayCast3D = $"RayCast3D"
+@onready var camera: Camera3D = $"../../Camera3D2"
+
+var carrot = load("res://Scenes/Game/carrot.tscn")
 
 func _ready() -> void:
-	if baby == true:
-		SPEED = 0.2
-		health = 1
-	else if boss == true:
-		SPEED = 0.11
-		health = 100
-	else:
-		SPEED = 0.1
-		health = 3
+	pass
 
 # moves towards the player every frame
 func _physics_process(delta: float) -> void:
@@ -26,7 +24,6 @@ func _physics_process(delta: float) -> void:
 	velocity = target - position
 	velocity = velocity.normalized() * SPEED
 	look_at(target, Vector3.UP)
-	rotate_y(-PI/2) # the gingerbread model isn't forward by default
 	
 	var collision = move_and_collide(velocity)
 	
@@ -34,6 +31,15 @@ func _physics_process(delta: float) -> void:
 	if collision is KinematicCollision3D:
 		if collision.get_collider() == skater:
 			skater._take_damage(1)
+
+	if timer.is_stopped():
+		var instance = carrot.instantiate()
+		instance.position = position
+		shooter.global_transform.origin = target + Vector3(0, 1, 0)
+		instance.transform.basis = shooter.global_transform.basis
+		
+		get_parent().get_parent().add_child(instance)
+		timer.start(ATTACK_DELAY)
 	
 
 func _take_damage(damage: int) -> void:
